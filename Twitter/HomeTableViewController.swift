792c2c52e9f02bec75804efb2fa3b6b1e6c09023
Sunstar4 <stse3@mail.ccsf.edu>
2021-03-04 22:44:59 -0800

    Adjust gap between Labels, add Pull to Refresh & Infinite Scroll

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -11,24 +11,51 @@ import UIKit
 class HomeTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
-    var numberOfTweet = Int?.self
+    var numberOfTweet: Int!
+    //add refresh control
+    let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        //add below to see the loading wheel
+        tableView.refreshControl = myRefreshControl
     }
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    
-    func loadTweet(){
-
-        let myurl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+    @objc func loadTweets(){
         
+        //when we first load tweets, instead calling the number, we set it to 20
+        numberOfTweet = 20
+        
+        let myurl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweet]
+        
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myurl, parameters: myParams as [String: Any], success: {(tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            //after get the lists, update table, after done update the table, then refresh the table
+            self .myRefreshControl.endRefreshing()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets! uh oh!")
+        })
+        
+    }
+    
+    //add more tweet when user scroll down the list, first 20, then 40 add 20...etc..sth happen over & over again
+    func loadMoreTweets() {
+        
+        let myurl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        //we call 20, 40, 60 etc here
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myurl, parameters: myParams, success: {(tweets: [NSDictionary]) in
             
@@ -38,12 +65,22 @@ class HomeTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            // self .myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             print("Could not retreive tweets! uh oh!")
         })
-        
     }
+    //below code use to trigger sth. to happen when user scroll the end of the page. When it gets to the end of the page, load more tweets.
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
+    
     
     @IBAction func onLogoutBarButton(_ sender: Any) {
         TwitterAPICaller.client?.logout()
@@ -56,6 +93,7 @@ class HomeTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
         //extract the user, one single tweet at a time
         let user = tweetArray[indexPath.row]["user"] as! NSDictionary?
+        
         cell.userNameLabel.text = (user!["name"] as! String)
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
         
@@ -137,3 +175,4 @@ class HomeTableViewController: UITableViewController {
      */
     
 }
+
